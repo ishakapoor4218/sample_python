0d501af0943d63e2cbdcff7744456b83f612a381
Isha Kapoor <ishakapoor4218@gmail.com>
2021-05-21 22:18:44 -0400

    My first Pytohn code

---
+++ first.py
@@ -0,0 +1,15 @@
+print('Isha Kapoor') #this command is used to print anything
+print('Welcome to Python')
+print('Happy Sunday')
+num1 = 10
+num2 = 20
+num3 = (num1+num2)/2
+total = 0
+print(num3)
+total = 0
+for x in range(1,6):
+    print(x)
+    t1 = total + x
+    total = t1
+avg = total/x
+print(avg)
